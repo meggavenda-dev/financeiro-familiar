8c3f4e151d335c7a6e8132e44648d0f0632ffd10
meggavenda-dev <meggavenda@gmail.com>
2026-01-07 15:03:16 -0300

    Update Usuarios.py

--- Usuarios.py
+++ Usuarios.py
@@ -1,1 +1,17 @@
 
+# pages/4_Usuarios.py
+import streamlit as st
+from github_service import GitHubService
+
+st.title("👥 Usuários")
+
+gh = GitHubService(
+    token=st.secrets["github_token"],
+    repo_full_name=st.secrets["repo_full_name"]
+)
+
+usuarios, sha = gh.get_json("data/usuarios.json", [])
+
+for u in usuarios:
+    st.write(f"**{u['nome']}** — Perfil: {u['perfil']}")
+
