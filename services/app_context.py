60321aaeee8f21f86e3d8816586a74687e97e66d
meggavenda-dev <meggavenda@gmail.com>
2026-01-07 15:25:13 -0300

    Update app_context.py

--- services/app_context.py
+++ services/app_context.py
@@ -1,1 +1,19 @@
 
+import streamlit as st
+from github_service import GitHubService
+
+def get_context():
+    if "gh" not in st.session_state:
+        gh = GitHubService(
+            token=st.secrets["github_token"],
+            repo_full_name=st.secrets["repo_full_name"],
+            branch=st.secrets.get("branch_name", "main")
+        )
+        st.session_state.gh = gh
+
+    return {
+        "gh": st.session_state.gh,
+        "usuario_id": st.session_state.get("usuario_id", "u1"),
+        "perfil": st.session_state.get("perfil", "admin"),
+    }
+
